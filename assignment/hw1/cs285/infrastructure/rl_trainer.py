8908955cf7f4a81d8ce4c18f63f01c125babd6ed
JialeZhaAcademic <jiale.zha.academic@gmail.com>
2023-04-05 16:37:04 -0400

    update eval batch size

--- assignment/hw1/cs285/infrastructure/rl_trainer.py
+++ assignment/hw1/cs285/infrastructure/rl_trainer.py
@@ -226,7 +226,8 @@ class RL_Trainer(object):
 
         # collect eval trajectories, for logging
         print("\nCollecting data for eval...")
-        eval_paths, eval_envsteps_this_batch = utils.sample_trajectories(self.env, eval_policy, self.params['eval_batch_size'], self.params['ep_len'])
+        eval_paths, eval_envsteps_this_batch = utils.sample_trajectories(
+            self.env, eval_policy, self.params['eval_batch_size']*self.params['ep_len'], self.params['ep_len'])
 
         # save eval rollouts as videos in tensorboard event file
         if self.log_video and train_video_paths != None:

ebbf6f1fd1f5433417241e573d01a2b7c7b18a05
JialeZhaAcademic <jiale.zha.academic@gmail.com>
2023-04-23 16:35:05 -0400

    update sac agent

--- assignment/hw4/cs285/agents/sac_agent.py
+++ assignment/hw4/cs285/agents/sac_agent.py
@@ -7,6 +7,7 @@ import gym
 from cs285.policies.sac_policy import MLPPolicySAC
 from cs285.critics.sac_critic import SACCritic
 import cs285.infrastructure.pytorch_util as ptu
+import torch
 
 class SACAgent(BaseAgent):
     def __init__(self, env: gym.Env, agent_params):
@@ -42,8 +43,35 @@ class SACAgent(BaseAgent):
         self.training_step = 0
         self.replay_buffer = ReplayBuffer(max_size=100000)
 
-    def update_critic(self):
+    def update_critic(self, ob_no, ac_na, next_ob_no, re_n, terminal_n):
         # TODO: get this from previous HW  
+        # 1. Compute the target Q value. 
+        # HINT: You need to use the entropy term (alpha)
+        # 2. Get current Q estimates and calculate critic loss
+        # 3. Optimize the critic
+
+        ob_no = ptu.from_numpy(ob_no)
+        ac_na = ptu.from_numpy(ac_na)
+        next_ob_no = ptu.from_numpy(next_ob_no)
+        re_n = ptu.from_numpy(re_n)
+        terminal_n = ptu.from_numpy(terminal_n)
+
+        Q1, Q2 = self.critic(ob_no, ac_na)
+
+        ac_tp1_dist = self.actor(next_ob_no)
+        ac_tp1 = ac_tp1_dist.sample()
+        log_pi = ac_tp1_dist.log_prob(ac_tp1)
+
+        Q_stp1, _ = torch.min(self.critic_target(next_ob_no, ac_tp1))
+
+        target = re_n + self.gamma * (1 - terminal_n) * (Q_stp1 - self.actor.alpha * log_pi)
+        critic_loss = self.critic.loss(Q1, target)
+        critic_loss += self.critic.loss(Q2, target)
+
+        self.critic.optimizer.zero_grad()
+        self.critic.backward()
+        self.critic.optimizer.step()
+
         return critic_loss
 
     def train(self, ob_no, ac_na, re_n, next_ob_no, terminal_n):
